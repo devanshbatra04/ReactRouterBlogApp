66579d6ca82da146f575792e32a6ea7a4b7a796d
devanshbatra04 <devanshbatra04@gmail.com>
2018-06-15 15:35:13 +0530

    touch property added to validations

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -10,7 +10,7 @@ class PostsNew extends Component {
                     {...field.input}
                 />
                 {
-                    field.meta.error
+                    field.meta.touched ? field.meta.error : ''
                 }
             </div>
         )
