a0fca29b62d722149156b9a3cf41ab1f0134f834
devanshbatra04 <devanshbatra04@gmail.com>
2018-06-15 15:17:47 +0530

    validation added

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -22,8 +22,13 @@ class PostsNew extends Component {
                     component={this.renderField}
                 />
                 <Field
-                    label="Tags"
-                    name="tags"
+                    label="Categories"
+                    name="categories"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Content"
+                    name="content"
                     component={this.renderField}
                 />
             </form>
@@ -31,8 +36,29 @@ class PostsNew extends Component {
     }
 }
 
+function validate(values){
+    // console.log(values) -> {title:'asdf', categories:'asdf'}
+    // validate inputs from 'values';
+    const errors ={};
+
+    if (!values.title || values.title.length < 3){
+        errors.title = "Enter a title at least 3 characters!";
+    }
+    if (!values.categories){
+        errors.categories = "Enter a title at least 3 characters!";
+    }
+    if (!values.content){
+        errors.content = "Enter a title at least 3 characters!";
+    }
+
+    //if errors is empty submit,
+    //if errors has any properties, redux assumes that the form is invalide
+    return errors
+}
+
 // redux form to redux conect
 export default reduxForm({
+    validate,
     form: 'PostNewForm',
 
 })(PostsNew);
