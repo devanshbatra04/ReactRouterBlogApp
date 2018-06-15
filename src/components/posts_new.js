95de6b2498f9aaa6c837cfebda4cfc22b12d3613
devanshbatra04 <devanshbatra04@gmail.com>
2018-06-15 14:57:28 +0530

    tags field added

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,23 +2,30 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-    renderTitleField(field){
+    renderField(field){
         return(
-            <div>
-                <input
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input className="form-control"
                     {...field.input}
                 />
             </div>
         )
     }
+
     render(){
         return (
             <form>
                 <Field
+                    label="Title"
                     name="title"
-                    component={this.renderTitleField}
+                    component={this.renderField}
                 />
-
+                <Field
+                    label="Tags"
+                    name="tags"
+                    component={this.renderField}
+                />
             </form>
         )
     }
