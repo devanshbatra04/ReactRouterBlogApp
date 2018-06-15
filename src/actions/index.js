8b47a1043a3dbc6e0a752a9d09312f7ccc5862d3
devanshbatra04 <devanshbatra04@gmail.com>
2018-06-15 17:47:01 +0530

    added show route

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = "FETCH_POSTS";
 export const CREATE_POST = "CREATE_POST";
+export const FETCH_POST = "FETCH_POST";
 
 const rootUrl = "http://reduxblog.herokuapp.com/api/";
 const API_KEY = '?key=Jamma1234';
@@ -20,6 +21,13 @@ export function createPost(values, callback){
         .then( () => callback());
     return {
         type: CREATE_POST,
+        payload: request
+    }
+}
+export function fetchPost(id){
+    const request = axios.get(`${rootUrl}/posts/${id}${API_KEY}`);
+    return {
+        type: FETCH_POSTS,
         payload: request
     }
 }
